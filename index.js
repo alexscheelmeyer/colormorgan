3bbbc6f6348fca173563c9a39eaf3fdbcd042dac
Alex Scheel Meyer <alexscheelmeyer@gmail.com>
2018-12-10 21:42:03 +0100

    version 0.0.2

--- index.js
+++ index.js
@@ -19,17 +19,13 @@ function colorStatuscode(code) {
 
 const morganMiddleware = morgan(function (tokens, req, res) {
     return [
-        // '\n\n\n',
         chalk.hex('#f78fb3').bold((new Date(tokens.date(req, res))).toISOString()),
-        // chalk.hex('#ff4757').bold('🍄  Morgan --> '),
         chalk.hex('#34ace0').bold(tokens.method(req, res)),
         colorStatuscode(tokens.status(req, res)),
         chalk.hex('#ff5252').bold(tokens.url(req, res)),
         chalk.hex('#2ed573').bold(tokens['response-time'](req, res) + 'ms'),
         chalk.yellow(tokens['remote-addr'](req, res)),
-        // chalk.hex('#fffa65').bold('from ' + tokens.referrer(req, res)),
         chalk.hex('#1e90ff')(tokens['user-agent'](req, res)),
-        // '\n\n\n',
     ].join(' ');
 });
 
